ea41304a82c4616d49d0eaaf9b9e74aa62af7f88
hsimao <e087754958@gmail.com>
2019-03-17 22:36:22 +0800

    add: 後端取得url query參數重新獲取下一頁api資料

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import fetch from 'isomorphic-unfetch'
 import Error from 'next/error'
+import Link from 'next/link'
 
 import Layout from '../components/Layout'
 import StoryList from '../components/StoryList'
@@ -7,20 +8,23 @@ import StoryList from '../components/StoryList'
 const hackerNewsAPI = 'https://node-hnapi.herokuapp.com/news'
 
 class Index extends React.Component {
-  static async getInitialProps() {
+  static async getInitialProps({ req, res, query }) {
     let stories
+    let page
+
     try {
-      const res = await fetch(`${hackerNewsAPI}?page=1`)
+      page = Number(query.page) || 1
+      const res = await fetch(`${hackerNewsAPI}?page=${page}`)
       stories = await res.json()
     } catch (err) {
       console.log(err)
       stories = []
     }
-    return { stories }
+    return { stories, page }
   }
 
   render() {
-    const { stories } = this.props
+    const { stories, page } = this.props
 
     if (stories.length === 0) {
       return <Error statusCode={503} />
@@ -29,6 +33,24 @@ class Index extends React.Component {
     return (
       <Layout title="Hacker Next" description="A Hacker News clone made with Next.js">
         <StoryList stories={stories} />
+
+        <footer>
+          <Link href={`/?page=${page + 1}`}>
+            <a>Next Page ({page + 1})</a>
+          </Link>
+        </footer>
+
+        <style jsx>{`
+          footer {
+            padding: 1em;
+          }
+
+          footer a {
+            font-weight: bold;
+            color: black;
+            text-decoration: none;
+          }
+        `}</style>
       </Layout>
     )
   }
